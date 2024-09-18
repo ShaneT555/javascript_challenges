d38da438a5ccf879f2d359c7314a78dcc7870eb7
Shane <179925883+ShaneT555@users.noreply.github.com>
2024-09-18 20:57:07 +0100

    day2activity10-added

---
+++ day2-activity10.js
@@ -0,0 +1,18 @@
+//Create an aaray that lists your favourite films, up to 5 elemets. 
+
+let favFilms = ["Lion King",
+    "Count of Monte Cristo",
+    "Matrix",
+    "Blankman",
+    "How High",
+];
+
+// add 2 more using a method
+
+favFilms.push("Sherloke Holmes", "Lord of the Rings: Fellowship"); 
+
+//Use a loop to cycle through the array
+
+for (let i = 0; i < favFilms.length; i++) {
+    console.log(favFilms[i])
+}
